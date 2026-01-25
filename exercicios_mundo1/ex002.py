cf63e81fe43a1752bbcba5a06b702df2a2adcc83
Letícia El Kadre <leticiamkadre@gmail.com>
2026-01-24 22:49:17 -0300

    Atualizar o ex002.py

--- exercicios_mundo1/ex002.py
+++ exercicios_mundo1/ex002.py
@@ -1,3 +1,3 @@
 # Exercício 2: imprimir uma frase de boas-vindas para um nome fornecido pelo usuário.
 nome = input('Qual é seu nome?')
-print('Que bom te conhecer, {}!'.format(nome))
+print(f'Que bom te conhecer, {nome}!')
